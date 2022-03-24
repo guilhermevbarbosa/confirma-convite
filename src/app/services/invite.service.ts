525754143cbb9eff4db67a0f17dad17d67ba0a8f
Guilherme Vinicius Barbosa <guilhermevbarbosa1999@gmail.com>
2022-03-24 17:54:57 -0300

    Confirmar presença

--- src/app/services/invite.service.ts
+++ src/app/services/invite.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 
-import { Firestore, doc, deleteDoc, collection, setDoc } from '@angular/fire/firestore';
+import { Firestore, doc, deleteDoc, collection, setDoc, query, where, getDoc, getDocs } from '@angular/fire/firestore';
+import { updateDoc } from 'firebase/firestore';
 
 import { Invite } from '../models/invite.model';
 
@@ -11,26 +12,52 @@ export class InviteService {
 
   private fsRef: Firestore;
   private CHILD_INVITES = '/invites';
-  private cities: any = [];
+  private collectionRef: any;
 
   constructor(firestore: Firestore) {
     this.fsRef = firestore;
+    this.collectionRef = collection(this.fsRef, this.CHILD_INVITES);
   }
 
   delete(uid: string) {
-    const document = doc(this.fsRef, this.CHILD_INVITES, uid);
+    const document = doc(this.collectionRef, uid);
     return deleteDoc(document);
   }
 
   create(invite: Invite) {
-    const uid = doc(collection(this.fsRef, this.CHILD_INVITES)).id;
+    const uid = doc(this.collectionRef).id;
 
-    return setDoc(doc(this.fsRef, this.CHILD_INVITES, uid), {
+    return setDoc(doc(this.collectionRef, uid), {
       uid,
       code: invite.code,
       name: invite.name,
       amount: invite.amount,
       confirmed: false,
     });
+  }
+
+  async activateInvite(inviteCode: string) {
+    let searchedInvite: any;
+    let searchedInviteUid: any;
+    const q: any = query(this.collectionRef, where("code", "==", inviteCode));
+
+    const docSnap = await getDocs(q);
+    docSnap.forEach((doc) => {
+      searchedInvite = doc.data();
+      searchedInviteUid = searchedInvite.uid;
+    });
+
+    if (searchedInvite == undefined) {
+      throw "Código inválido";
+    }
+
+    if (searchedInvite.confirmed) {
+      throw "Convite já confirmado";
+    } else {
+      const document = doc(this.collectionRef, searchedInviteUid);
+      return updateDoc(document, {
+        confirmed: true
+      })
+    }
   }
 }
--- src/app/views/confirm-invite/confirm-invite.component.ts
+++ src/app/views/confirm-invite/confirm-invite.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { InviteService } from 'src/app/services/invite.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-confirm-invite',
@@ -21,10 +22,33 @@ export class ConfirmInviteComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getInviteData();
   }
 
-  async getInviteData() {
-    // this.invite = this.iS.confirmInvite('66zae');
+  activateInvite() {
+    if (this.inviteCode.length < 5) {
+      this.inviteCodeErrorMessage = "O código deve ter 5 caracteres.";
+    } else {
+      this.inviteCodeErrorMessage = '';
+      this.loading = true;
+
+      this.iS.activateInvite(this.inviteCode)
+        .then(() => {
+          this.loading = false;
+
+          Swal.fire(
+            'Que legal!',
+            'Presença confirmada com sucesso',
+            'success'
+          );
+        }).catch((error) => {
+          this.loading = false;
+
+          Swal.fire(
+            'Erro!',
+            error,
+            'error'
+          );
+        })
+    }
   }
 }
