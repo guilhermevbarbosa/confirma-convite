f8133f28246900e88e38e68cb0d62c3aec3f243f
Guilherme Vinicius Barbosa <31224718+guilhermevbarbosa@users.noreply.github.com>
2022-03-24 21:13:22 -0300

    firebase-config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAU6dVxZgnK6h75W9_payXNZdcFJl4xHc8",
+    authDomain: "convite-prod.firebaseapp.com",
+    projectId: "convite-prod",
+    storageBucket: "convite-prod.appspot.com",
+    messagingSenderId: "235461613366",
+    appId: "1:235461613366:web:f16d9ea66c7fbfb40b0597"
+  }
 };
