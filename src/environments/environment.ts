417df62c35aeafd1be1661039939a2f926c174b1
Guilherme Vinicius Barbosa <31224718+guilhermevbarbosa@users.noreply.github.com>
2024-05-08 11:15:20 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDueyFkwBYj-K0kFwuTgMymaiZxqb78ya8",
-    authDomain: "con-vite.firebaseapp.com",
-    databaseURL: "https://con-vite-default-rtdb.firebaseio.com",
-    projectId: "con-vite",
-    storageBucket: "con-vite.appspot.com",
-    messagingSenderId: "180932699562",
-    appId: "1:180932699562:web:44036b7c7bc2da162477d3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
