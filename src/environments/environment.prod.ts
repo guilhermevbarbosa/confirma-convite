9e9f08c181adb4939c061ffceb03a371fd317f86
Guilherme Vinicius Barbosa <31224718+guilhermevbarbosa@users.noreply.github.com>
2024-05-08 11:14:56 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBFAp2tdCDQnPUUH-M8tOXuZVevGl6pGes",
-    authDomain: "festa-gabi.firebaseapp.com",
-    projectId: "festa-gabi",
-    storageBucket: "festa-gabi.appspot.com",
-    messagingSenderId: "495339760063",
-    appId: "1:495339760063:web:f82880eb5dc9827d1f990a"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   }
 };
